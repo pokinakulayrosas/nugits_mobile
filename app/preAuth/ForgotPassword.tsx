024c6745fda985e5cfc44e7a4784196c2e14f587
Lex <lexamalik17@gmail.com>
2025-03-11 13:57:02 +0800

    try

---
+++ app/preAuth/ForgotPassword.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class ForgotPassword extends Component {
+  render() {
+    return (
+      <View className='flex-1 px-6 justify-center'>
+        <Text>ForgotPassword</Text>
+      </View>
+    )
+  }
+}
